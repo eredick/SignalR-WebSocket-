c48d7a296a706d9e30466c946baff6a459dc5bb3
erick g <edu2726@gmail.com>
2018-05-10 20:33:48 -0500

    Agregar archivos de proyecto.

---
+++ SignalRDemo.Web/Hubs/ContadorHub.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+using Microsoft.AspNet.SignalR.Hubs;
+
+namespace SignalRDemo.Web.Hubs
+{
+    [HubName("contador")]
+    public class ContadorHub : Hub
+    {
+        static int _contador = 0;
+        //public void Hello()
+        //{
+        //    Clients.All.hello();
+        //}
+        public void AumentarContador()
+        {
+            _contador++;
+            Clients.All.onActualizarContador(new { contador = _contador });
+        }
+
+        public override Task OnDisconnected(bool stopCalled)
+        {
+            _contador--;
+            Clients.All.onActualizarContador(new { contador = _contador });
+            return base.OnDisconnected(stopCalled);
+        }
+    }
+}
